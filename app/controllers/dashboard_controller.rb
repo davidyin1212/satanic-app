d6b7932b07129b534e994d7dc1fb4d4183ad0c09
davidyin1212 <davidyin1212@gmail.com>
2016-01-24 04:22:56 -0500

    seed data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,43 +1,43 @@
 class DashboardController < ApplicationController
 	def index
-		# @friends = current_user.twitter.friends()
-		# @friendsList = []
-		# @tweetsList = []
-		# @friends.each do |friend_id|
-		# 	friend = Friend.find_by(uid: friend_id.id)
-		# 	if friend == nil
-		# 		friend = Friend.new()
-		# 		friend.uid = friend_id.id
-		# 		friend.name = friend_id.name
-		# 		friend.save
-		# 	end
-		# 	puts "friend: " + friend.to_s 
-		# 	@friendsList.push(friend)
-		# 	tweets = friend.getTweets(current_user.twitter)
-		# 	tweets.each do |tweet|
-		# 		if not Tweet.where({:user_id => current_user.id, :uid => tweet.id}).empty?
-		# 			puts "tweets already exists"
-		# 			break
-		# 		end
-		# 		tweet_obj = Tweet.new()
-		# 		tweet_obj.post_name = friend.name
-		# 		tweet_obj.uid = tweet.id
-		# 		tweet_obj.user_id = current_user.id
-		# 		tweet_obj.body = tweet.text
-		# 		if tweet.media[0]
-		# 			puts "-----text media: " + tweet.media[0].media_url
-		# 			tweet_obj.getMedia(tweet.media[0].media_url)
-		# 		end
-		# 		tweet_obj.save
-		# 		@tweetsList.push(tweet_obj)
-		# 	end
-		# end
-		# puts "tweets list: " + @tweetsList.to_s
-		# current_user.tweets | @tweetsList
-		# puts "friendList: " + @friendsList.to_s
-	# current_user.friends = @friendsList
-		# current_user.save
-		# #download
+		@friends = current_user.twitter.friends()
+		@friendsList = []
+		@tweetsList = []
+		@friends.each do |friend_id|
+			friend = Friend.find_by(uid: friend_id.id)
+			if friend == nil
+				friend = Friend.new()
+				friend.uid = friend_id.id
+				friend.name = friend_id.name
+				friend.save
+			end
+			puts "friend: " + friend.to_s 
+			@friendsList.push(friend)
+			tweets = friend.getTweets(current_user.twitter)
+			tweets.each do |tweet|
+				if not Tweet.where({:user_id => current_user.id, :uid => tweet.id}).empty?
+					puts "tweets already exists"
+					break
+				end
+				tweet_obj = Tweet.new()
+				tweet_obj.post_name = friend.name
+				tweet_obj.uid = tweet.id
+				tweet_obj.user_id = current_user.id
+				tweet_obj.body = tweet.text
+				if tweet.media[0]
+					puts "-----text media: " + tweet.media[0].media_url
+					tweet_obj.getMedia(tweet.media[0].media_url)
+				end
+				tweet_obj.save
+				@tweetsList.push(tweet_obj)
+			end
+		end
+		puts "tweets list: " + @tweetsList.to_s
+		current_user.tweets | @tweetsList
+		puts "friendList: " + @friendsList.to_s
+		current_user.friends = @friendsList
+		current_user.save
+		#download
 		puts "here are the params: " + params.to_s
 		#get tweets on date
 		if params[:date] == nil
